2e025c28204c84d9d71475e8174cd19fc772e27c
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-07-22 23:28:09 -0300

    2025-07-22 Fixed Alexandrite block texture

--- src/main/java/net/karen/mccoursemod/datagen/ModItemModelGenerators.java
+++ src/main/java/net/karen/mccoursemod/datagen/ModItemModelGenerators.java
@@ -8,14 +8,8 @@ import net.minecraft.client.data.models.model.ItemModelUtils;
 import net.minecraft.client.data.models.model.ModelInstance;
 import net.minecraft.client.data.models.model.ModelLocationUtils;
 import net.minecraft.client.data.models.model.ModelTemplates;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.level.block.Block;
-
-import java.util.List;
 import java.util.function.BiConsumer;
-import static net.karen.mccoursemod.datagen.ModModelProvider.*;
 
 public class ModItemModelGenerators extends ItemModelGenerators {
     public ModItemModelGenerators(ItemModelOutput itemModelOutput,
@@ -29,12 +23,9 @@ public class ModItemModelGenerators extends ItemModelGenerators {
         // Custom items
         this.generateFlatItem(ModItems.ALEXANDRITE.get(), ModelTemplates.FLAT_ITEM);
         // Custom blocks
-        this.generateFlatItem(ModBlocks.ALEXANDRITE_BLOCK.get().asItem(), ModelTemplates.FLAT_ITEM);
-//        this.generateFlatItem(ModBlocks.ENCHANT.get().asItem(), ModelTemplates.FLAT_ITEM);
+        this.itemModelOutput.accept(ModBlocks.ALEXANDRITE_BLOCK.get().asItem(),
+                                    ItemModelUtils.plainModel(ModelLocationUtils.getModelLocation(ModBlocks.ALEXANDRITE_BLOCK.get())));
         this.itemModelOutput.accept(ModBlocks.ENCHANT.get().asItem(),
-                ItemModelUtils.plainModel(ModelLocationUtils.getModelLocation(ModBlocks.ENCHANT.get())));
-        if (this.itemModelOutput instanceof ItemInfoCollector collector) {
-                collector.generateDefaultBlockModels();
-        }
+                                    ItemModelUtils.plainModel(ModelLocationUtils.getModelLocation(ModBlocks.ENCHANT.get())));
     }
 }
